5f13f6ef26c10a9c784429bf3f3aa1d821cbffa2
Anton McConville <anton@hickory.ca>
2014-01-03 12:39:47 -0500

    Restructuring pages

--- javascript/blog.js
+++ javascript/blog.js
@@ -10,7 +10,7 @@ function plot( data ){
 	var d = new Date(data.date);
     var dateString = moment(d).format('MMMM Do YYYY');
 
-	var postContent = '<span><!-- span class="entry">' + data.id + '</span --><h3 class="title">' + data.title + '</h3></span>' +
+	var postContent = '<span><!-- span class="entry">' + data.id + '</span --><h3 class="blogtitle">' + data.title + '</h3></span>' +
 						'<p class="metadata">Entry ' + data.id + ' - Posted by ' + data.author + ' on ' + dateString + '</p>' +
 						' <div class="post-content">' + content + '</div>' +
 						'<p class="stats">Hours worked: ' + data.hoursworked + '</p>';
@@ -21,11 +21,11 @@ function plot( data ){
 }
 
 function showInfo(data) {
-	var reversed = data.SBNotes.elements.reverse();
+	var reversed = data.BlogNotes.elements.reverse();
     reversed.forEach( plot );
 }
 
 window.onload = function() {
-    var spreadsheet = 'https://docs.google.com/spreadsheet/pub?key=0AhLgoEUzhCg_dGlDTGlIakplSE5UQ1hTamNiaEI1blE&output=html';
+    var spreadsheet = 'https://docs.google.com/spreadsheet/pub?key=0AhLgoEUzhCg_dEFncTByWTJJLVFVQWstRTBlcGt6WWc&output=html';
     Tabletop.init({ key: spreadsheet, callback: showInfo });
 };
